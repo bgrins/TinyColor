686ae92fcec219dca43f7d5abd10925fd96681f9
bgrins <briangrinstead@gmail.com>
2011-06-30 09:03:23 -0500

    dont declare function inside conditional block

--- tinycolor.js
+++ tinycolor.js
@@ -126,17 +126,18 @@ function rgbToHsl(r, g, b){
 function hslToRgb(h, s, l){
     var r, g, b;
 
+    function hue2rgb(p, q, t){
+        if(t < 0) t += 1;
+        if(t > 1) t -= 1;
+        if(t < 1/6) return p + (q - p) * 6 * t;
+        if(t < 1/2) return q;
+        if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+        return p;
+    }
+    
     if(s == 0){
         r = g = b = l; // achromatic
     }else{
-        function hue2rgb(p, q, t){
-            if(t < 0) t += 1;
-            if(t > 1) t -= 1;
-            if(t < 1/6) return p + (q - p) * 6 * t;
-            if(t < 1/2) return q;
-            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-            return p;
-        }
 
         var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
         var p = 2 * l - q;
