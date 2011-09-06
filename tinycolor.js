990f955e1b4d0ff5e31405a9ca28f36c4133db3f
bgrins <briangrinstead@gmail.com>
2011-09-06 12:33:08 -0500

    minor version

--- tinycolor.js
+++ tinycolor.js
@@ -1,9 +1,9 @@
-// TinyColor.js - https://github.com/bgrins/TinyColor - 2011 Brian Grinstead - v0.3
+// TinyColor.js - https://github.com/bgrins/TinyColor - 2011 Brian Grinstead - v0.3.1
 
 var tinycolor = (function() {
 
 var tc = _tinycolor;
-tc.version = "0.3";
+tc.version = "0.3.1";
 tc.equals = function(color1, color2) {
 	return tc(color1).toHex() == tc(color2).toHex();
 }
