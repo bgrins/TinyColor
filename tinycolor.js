168874e8ddc3345dbba1605a670da5a105793d11
Brian Grinstead <briangrinstead@gmail.com>
2011-09-13 16:09:31 -0500

    Edited tinycolor.js via GitHub

--- tinycolor.js
+++ tinycolor.js
@@ -509,7 +509,8 @@ function bound01(n, max) {
 		n = n * (max / 100);
 	}
 	
-	if (n == max) {
+	// Handle floating point rounding errors
+	if ((Math.abs(n - max) < 0.000001)) {
 		return 1;
 	}
 	else if (n > 1) {
