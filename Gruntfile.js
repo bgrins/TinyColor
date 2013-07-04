6bf81b11a9d3637a70f28068c5f9608a221c4de3
bgrins <briangrinstead@gmail.com>
2013-07-04 14:23:05 -0500

    update gruntfile and rebuild

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,20 +35,23 @@ module.exports = function(grunt) {
         }
       },
       all: ['tinycolor.js']
+    },
+
+    docco: {
+      debug: {
+        src: ['tinycolor.js'],
+        options: {
+          output: 'docs/'
+        }
+      }
     }
   });
 
 
-  grunt.registerTask('docco', 'Annotate the source.', function(options) {
-    grunt.utils.spawn({
-      cmd: "docco",
-      args: ['-o', 'docs', 'tinycolor.js']
-    });
-  });
-
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-docco');
 
   grunt.registerTask('default', ['jshint']);
   grunt.registerTask('build', ['jshint', 'uglify', 'docco']);
