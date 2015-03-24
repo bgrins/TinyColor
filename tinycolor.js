f9c534609cfd483e5549ef4b7d8f9b45a7f0148b
Brian Grinstead <briangrinstead@gmail.com>
2015-03-23 17:47:19 -0700

    whitespace

--- tinycolor.js
+++ tinycolor.js
@@ -503,21 +503,22 @@ function rgbToHex(r, g, b, allow3Char) {
 
     return hex.join("");
 }
-    // `rgbaToHex`
-    // Converts an RGBA color plus alpha transparency to hex
-    // Assumes r, g, b and a are contained in the set [0, 255]
-    // Returns an 8 character hex
-    function rgbaToHex(r, g, b, a) {
 
-        var hex = [
-            pad2(convertDecimalToHex(a)),
-            pad2(mathRound(r).toString(16)),
-            pad2(mathRound(g).toString(16)),
-            pad2(mathRound(b).toString(16))
-        ];
+// `rgbaToHex`
+// Converts an RGBA color plus alpha transparency to hex
+// Assumes r, g, b and a are contained in the set [0, 255]
+// Returns an 8 character hex
+function rgbaToHex(r, g, b, a) {
 
-        return hex.join("");
-    }
+    var hex = [
+        pad2(convertDecimalToHex(a)),
+        pad2(mathRound(r).toString(16)),
+        pad2(mathRound(g).toString(16)),
+        pad2(mathRound(b).toString(16))
+    ];
+
+    return hex.join("");
+}
 
 // `equals`
 // Can be called with any tinycolor input
