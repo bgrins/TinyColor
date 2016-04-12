6574f05f51993b5adfd9ec05ad45ee9a9b756896
Jurgen Westerhof <jurgen.westerhof@gmail.com>
2016-04-12 22:45:42 +0200

    Convenience functions
    Added more convenience functions like the existing triad and tetrad.

--- tinycolor.js
+++ tinycolor.js
@@ -255,12 +255,27 @@ tinycolor.prototype = {
     splitcomplement: function() {
         return this._applyCombination(splitcomplement, arguments);
     },
+    dyad: function() {
+        return this._applyCombination(nad, [2]);
+    },
     triad: function() {
         return this._applyCombination(nad, [3]);
     },
     tetrad: function() {
         return this._applyCombination(nad, [4]);
     }
+    pentad: function() {
+        return this._applyCombination(nad, [5]);
+    },
+    hexad: function() {
+        return this._applyCombination(nad, [6]);
+    },
+    heptad: function() {
+        return this._applyCombination(nad, [7]);
+    },
+    octad: function() {
+        return this._applyCombination(nad, [8]);
+    },
     nad: function(n) {
         return this._applyCombination(nad, [n])
     }
