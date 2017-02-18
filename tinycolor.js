fbb366dc1a2249d7c7a5e5d3cbb5fbb41c88d9e2
AngryAndConflict <arkadip13@gmail.com>
2017-02-18 21:16:49 +0300

    clamping 0 255 for brighten function

--- tinycolor.js
+++ tinycolor.js
@@ -590,9 +590,9 @@ function lighten (color, amount) {
 function brighten(color, amount) {
     amount = (amount === 0) ? 0 : (amount || 10);
     var rgb = tinycolor(color).toRgb();
-    rgb.r = mathMax(0, mathMin(255, rgb.r - mathRound(255 * - (amount / 100))));
-    rgb.g = mathMax(0, mathMin(255, rgb.g - mathRound(255 * - (amount / 100))));
-    rgb.b = mathMax(0, mathMin(255, rgb.b - mathRound(255 * - (amount / 100))));
+    rgb.r = clamp(0, 255, rgb.r - mathRound(255 * - (amount / 100)));
+    rgb.g = clamp(0, 255, rgb.g - mathRound(255 * - (amount / 100)));
+    rgb.b = clamp(0, 255, rgb.b - mathRound(255 * - (amount / 100)));
     return tinycolor(rgb);
 }
 
