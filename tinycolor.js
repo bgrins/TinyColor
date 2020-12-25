d8d2f182e5a86a3e0b92e7cd275e58f9b994780c
Jay Rizuri <65318393+JayRizuri@users.noreply.github.com>
2020-12-25 11:36:23 -0500

    Update tinycolor.js

--- tinycolor.js
+++ tinycolor.js
@@ -16,7 +16,7 @@ class TinyColor {
          this.color = (color) ? color : '';
          this.opts = opts || { };
 
-         var rgb = this.inputToRGB(color);
+         var rgb = inputToRGB(color);
          this.originalInput = color,
          this.red = rgb.r,
          this.green = rgb.g,
@@ -337,55 +337,55 @@ class TinyColor {
 
         var rgb = {
             r: 0,
-            g: 0,
-	        b: 0 
-	    },
-	    a = 1,
-	    s = null,
-	    v = null,
-	    l = null,
-	    ok = false,
-	    format = false;
-	
-	    if (typeof color == "string")
-	        color = stringInputToObject(color);
-	
-	    if (typeof color == "object") {
-	        if (isValidCSSUnit(color.r) && isValidCSSUnit(color.g) && isValidCSSUnit(color.b)) {
-	            rgb = rgbToRgb(color.r, color.g, color.b);
-	            ok = true;
-	            format = String(color.r).substr(-1) === "%" ? "prgb" : "rgb";
-	        }
-	        else if (isValidCSSUnit(color.h) && isValidCSSUnit(color.s) && isValidCSSUnit(color.v)) {
-	            s = convertToPercentage(color.s);
-	            v = convertToPercentage(color.v);
-	            rgb = hsvToRgb(color.h, s, v);
-	            ok = true;
-	            format = "hsv";
-	        }
-	        else if (isValidCSSUnit(color.h) && isValidCSSUnit(color.s) && isValidCSSUnit(color.l)) {
-	            s = convertToPercentage(color.s);
-	            l = convertToPercentage(color.l);
-	            rgb = hslToRgb(color.h, s, l);
-	            ok = true;
-	            format = "hsl";
-	        }
-	
-	        if (color.hasOwnProperty("a"))
-	            a = color.a;
-	    }
-	
-	    a = boundAlpha(a);
-	
-	    return {
-	        ok: ok,
-	        format: color.format || format,
-	        r: mathMin(255, mathMax(rgb.r, 0)),
-	        g: mathMin(255, mathMax(rgb.g, 0)),
-	        b: mathMin(255, mathMax(rgb.b, 0)),
-	        a: a
-	    };
-	}
+             g: 0,
+         b: 0 
+    },
+    a = 1,
+    s = null,
+    v = null,
+    l = null,
+    ok = false,
+    format = false;
+
+    if (typeof color == "string")
+        color = stringInputToObject(color);
+
+    if (typeof color == "object") {
+        if (isValidCSSUnit(color.r) && isValidCSSUnit(color.g) && isValidCSSUnit(color.b)) {
+            rgb = rgbToRgb(color.r, color.g, color.b);
+            ok = true;
+            format = String(color.r).substr(-1) === "%" ? "prgb" : "rgb";
+        }
+        else if (isValidCSSUnit(color.h) && isValidCSSUnit(color.s) && isValidCSSUnit(color.v)) {
+            s = convertToPercentage(color.s);
+            v = convertToPercentage(color.v);
+            rgb = hsvToRgb(color.h, s, v);
+            ok = true;
+            format = "hsv";
+        }
+        else if (isValidCSSUnit(color.h) && isValidCSSUnit(color.s) && isValidCSSUnit(color.l)) {
+            s = convertToPercentage(color.s);
+            l = convertToPercentage(color.l);
+            rgb = hslToRgb(color.h, s, l);
+            ok = true;
+            format = "hsl";
+        }
+
+        if (color.hasOwnProperty("a"))
+            a = color.a;
+    }
+
+    a = boundAlpha(a);
+
+    return {
+        ok: ok,
+        format: color.format || format,
+        r: mathMin(255, mathMax(rgb.r, 0)),
+        g: mathMin(255, mathMax(rgb.g, 0)),
+        b: mathMin(255, mathMax(rgb.b, 0)),
+        a: a
+    };
+}
 }
 
 
@@ -1225,11 +1225,11 @@ function validateWCAG2Parms(parms) {
 
 // Node: Export function
 if (typeof module !== "undefined" && module.exports) {
-    module.exports = tinycolor;
+    module.exports = TinyColor;
 }
 // AMD/requirejs: Define the module
-else if (typeof define === 'function' && define.amd) {
-    define(function () {return tinycolor;});
+else if (typeof define === 'class' && define.amd) {
+    define(function () {return TinyColor;});
 }
 // Browser: Expose to window
 else {
