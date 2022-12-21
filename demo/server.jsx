07a1f81c9b3182ded49f7866726b3fc8a76c7719
Brian Grinstead <briangrinstead@gmail.com>
2022-12-20 21:29:51 -0800

    serve static file

--- demo/server.jsx
+++ demo/server.jsx
@@ -3,21 +3,20 @@ import { ImageResponse } from "https://deno.land/x/og_edge/mod.ts";
 import { h } from "https://esm.sh/preact";
 import render from "https://esm.sh/preact-render-to-string";
 import { serve } from "https://deno.land/std@0.126.0/http/server.ts";
+import { serveFile } from "https://deno.land/std@0.140.0/http/file_server.ts";
 import tinycolor from "../mod.js";
 
-async function image_handler(req) {
+async function image_handler(req, input) {
+  const color = tinycolor(input || "lavender");
   const { searchParams } = new URL(req.url);
   const fontSize = searchParams.get("fontSize") || 60;
-  const backgroundColor = searchParams.get("backgroundColor") || "lavender";
   const width = searchParams.get("width") || 1200;
   const height = searchParams.get("height") || 627;
   const debug = searchParams.get("debug") || false;
 
-  const color = tinycolor(backgroundColor);
   if (!color.isValid()) {
     return new Response("Invalid color", { status: 400 });
   }
-  console.log(color, color.toHexString());
   let textColor = tinycolor.mostReadable(color, ["#333", "#ddd"]);
   return new ImageResponse(
     (
@@ -37,6 +36,7 @@ async function image_handler(req) {
         {/* <div>{color.toString()}</div>
         <div>{color.toHexString()}</div> */}
         {/* <div>{text}</div> */}
+        <div>{color.toName()}</div>
         <div>{color.toHexString()}</div>
         <div>{color.toRgbString()}</div>
         <div>{color.toHslString()}</div>
@@ -53,7 +53,22 @@ async function image_handler(req) {
 
 const port = 8080;
 const handler = (req) => {
-  return image_handler(req);
+  const url = new URL(req.url);
+  const { pathname } = url;
+  if (pathname === "/") {
+    return serveFile(req, "./index.html");
+  }
+  if (pathname === "/demo/demo.css") {
+    return serveFile(req, "./demo/demo.css");
+  }
+  if (pathname === "/tinycolor.js") {
+    return serveFile(req, "./tinycolor.js");
+  }
+  if (pathname.startsWith("/graphic/")) {
+    const input = pathname.replace("/graphic/", "");
+    return image_handler(req, input);
+  }
+  return new Response("Not Found", { status: 404 });
 };
 
 console.log(`HTTP webserver running. Access it at: http://localhost:8080/`);
